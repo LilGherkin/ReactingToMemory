972eef211a177415b69f2613934554d3f1f198d1
Mark Hartman <mthartman77@gmail.com>
2019-11-23 00:35:04 -0600

    Randomizer

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+//Call in everything we need.
+import React, {Component} from 'react';
 import './App.css';
+import Header from "./components/header";
+import Bigbox from "./components/bigbox";
+import Card from "./components/cards";
+import RememberThem from "./RememberThem.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Define a class that extends to all compoents in which it is called.
+class App extends Component {
+  state = {
+    pictures: RememberThem,
+    score: 0,
+    message: "",
+    tracker: []
+  };
+  // Creates a randomizer function that activates when an image is clicked.
+  randomizer = () => {
+    let pictures = this.state.pictures;
+    //For ease of calling.
+    let i = pictures.length;
+    //A real value
+    let value;
+    //Container value
+    let RandomizedIndex;
+    while (0 !== i) {
+      RandomizedIndex = Math.floor(Math.random() * i);
+      //Decrement index to eventually break while loop.
+      i -= 1;
+      //Set value equal to pictures at the index length;
+      value = pictures[i];
+      //
+      pictures[i] = pictures[RandomizedIndex];
+      pictures[RandomizedIndex] = value;
+    }
+    this.setState({pictures: pictures})
+  };
+
+  score = (id) => {
+    if (this.state.tracker.includes(id)) {
+      this.setState({
+        score: 0,
+        message: "Incorrect",
+        tracker: []
+      });
+    } else {
+      let value = this.state.tracker;
+      let highscore = this.state.highscore > this.state.score ? this.state.highscore : this.state.highscore + 1;
+      value.push(id);
+      this.setState({
+        score: this.state.score + 1,
+        highscore: highscore,
+        message: "Correct",
+        tracker: value
+      });
+    }
+  };
+
+  render(){
+    return(
+      <div>
+        <Header
+          message={this.state.message}
+          score={this.state.score}
+          highscore={this.state.highscore}
+        />
+        <Bigbox>
+          {this.state.pictures.map(image => (
+            <Card
+              id={image.id}
+              key={image.id}
+              name={image.name}
+              randomizer={this.randomizer}
+              score={this.score}
+            />
+          ))}
+        </Bigbox>
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ memory/src/components/bigbox/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./bigbox.css";
+
+const Bigbox = props => <div className="bigbox container">{props.children}</div>
+
+export default Bigbox;
