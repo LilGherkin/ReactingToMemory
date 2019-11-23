0dd31feb45da71e58d8c3ac46c49d213216803ee
Mark Hartman <mthartman77@gmail.com>
2019-11-23 00:05:48 -0600

    Fleshed out components.

---
+++ memory/src/components/cards/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./card.css";
+
+const Card = props => {
+    return(
+        <div className="card" onClick={(event) => {props.shuffle(); props.score(props.id);}}>
+            <div className="imageframe">
+                {/* Image elements get rendered here, and linter requires alt text*/}
+                <img src={props.image} alt="Happy Linter" />
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ memory/src/components/header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./header.css";
+
+//Make a header with properties
+const Header = props => {
+    return (
+        //Bootstrap components to define everything.
+        <nav className="navbar sticky-top navbar-dark bg-dark">
+            <div className="col-12">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h3 className="section1">Cartoon Clicker</h2>
+                    </div>
+                    {/*
+                    If props.message indicates that you guessed correctly it sets the class to our Success class, or our Failure class.
+                    */}
+                    <div className="col-md-4">
+                        <h2 className={props.message ==="Correct" ? "section2 Success" : "section2 Failure"}>{props.message}</h2>
+                    </div>
+                    <div className="col-md-4">
+                        <h2 className="section3">Score: {props.score} </h2>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
